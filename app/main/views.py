62081451df228f185056f73c7ad4bb9fd352b01b
mash <mashalonzo741@gmail.com>
2021-09-18 19:36:00 +0300

    Add the update profile page

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,9 @@
 from . import main
 from flask import render_template,request,redirect,url_for,abort
 from ..models import User
+from .forms import UpdateProfile
+from .. import db
+from flask_login import login_required, current_user
 
 
 @main.route('/user/<uname>')
@@ -12,3 +15,24 @@ def profile(uname):
 
     title = f"{uname}'s profile page"
     return render_template("profile/profile.html", user = user, title = title)
+
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    if user is None:
+        abort(404)
+
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname=user.username))
+
+    title = 'Update bio'
+    return render_template('profile/update.html',form =form,title = title)
